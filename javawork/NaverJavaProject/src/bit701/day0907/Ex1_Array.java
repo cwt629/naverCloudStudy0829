390564f3090ad940185a7a4c0599fb23b2963966
wontae chang <cwt629@naver.com>
2023-09-07 10:41:54 +0900

    git 재연동과 심심해서 트리 만들기

--- javawork/NaverJavaProject/src/bit701/day0907/Ex1_Array.java
+++ javawork/NaverJavaProject/src/bit701/day0907/Ex1_Array.java
@@ -4,6 +4,26 @@ public class Ex1_Array {
 
 	public static void main(String[] args) {
 		System.out.println("배열 복습 예제");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 29; j++) {
+				char ch = (j >= 14 - i && j <= 14 + i)? '*' : ' ';
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 29; j++) {
+				char ch = (j >= 10 - i && j <= 18 + i)? '*' : ' ';
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 29; j++) {
+				char ch = (j >= 12 && j <= 16)? '|' : ' ';
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
 	}
-
 }
